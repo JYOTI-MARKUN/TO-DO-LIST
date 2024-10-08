f3f056212d642b4655e713403365e85cbdd8c930
JYOTI MARKUN <jyotimarkun1997@gmail.com>
2024-10-08 16:26:45 +0530

    make some changes in js file

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const submitButton = document.querySelector(".sub-btn");
 const taskList = document.querySelector(".all-tasklist");
 const taskAddition = document.querySelector(".taskaddition");
-
 const deleteTask = document.querySelectorAll(".delete");
 const eachTask = document.querySelector(".outer-div");
 
 // function to add new task
-function addTask(text) {
+function addTask(text, saveToLocalStorage = true) {
   // adding classes and properties in the new task added
-  if (taskAddition.value.length > 0) {
+  if (text.length > 0 || !saveToLocalStorage) {
     const newTask = document.createElement("div");
     newTask.classList.add("outer-div");
     const inputField = document.createElement("input");
@@ -19,7 +18,8 @@ function addTask(text) {
 
     // append newtask to the list
     newTask.appendChild(inputField);
-    // add edit button 
+
+    // add edit button
     const editButton = document.createElement("button");
     editButton.textContent = "Edit";
     editButton.classList.add("edit");
@@ -29,52 +29,102 @@ function addTask(text) {
     deleteButton.textContent = "Delete";
     deleteButton.classList.add("delete");
 
-    newTask.appendChild(inputField);
     newTask.appendChild(editButton);
     newTask.appendChild(deleteButton);
 
     taskList.appendChild(newTask);
     console.log(newTask);
+
+    // after adding task input should be again empty
     taskAddition.value = "";
+
+    if (saveToLocalStorage) {
+      addTaskToLocalStorage(text);
+    }
   }
-  
+
   const editBtns = document.querySelectorAll(".edit");
   const editableInput = document.querySelectorAll(".editable-input");
-  console.log(editableInput)
+  console.log(editableInput);
 
   // make input able on the click of edit button
   editBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       const currentInputField = e.target.previousElementSibling;
-      console.log(currentInputField); 
+      console.log(currentInputField);
 
-      editableInput.forEach((input) => {   
-          input.disabled = true;
+      editableInput.forEach((input) => {
+        input.disabled = true;
       });
       currentInputField.disabled = false;
     });
   });
 
-
   // delete functionality
-  const deletebtns = document.querySelectorAll(".delete")
-  deletebtns.forEach((deletebtn)=>{
-    deletebtn.addEventListener("click",(e)=>{
-     console.log(e.target)
-     console.log(e.target.parentElement)
-     taskList.removeChild(e.target.parentElement)
-    })
-  })
-
-  
+  const deletebtns = document.querySelectorAll(".delete");
+  deletebtns.forEach((deletebtn) => {
+    deletebtn.addEventListener("click", (e) => {   
+      taskElement = e.target.parentElement
+      taskList.removeChild(taskElement);
+      removeTaskFromLocalStorage(taskElement)
+    });
+  });
 }
+
+
+// adding task to local storage
+function addTaskToLocalStorage(task) {
+  console.log(task);
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  console.log(tasks);
+  if (tasks.includes(task)) {
+    console.log(task);
+    console.log(`${task} is already added in the local storage`);
+  } else {
+    tasks.push(task);
+    console.log(task);
+    console.log(`${task} added to the local storage`);
+  }
+
+  let addedtasks = localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log(addedtasks);
+}
+
+// remove taks from localStorage
+function removeTaskFromLocalStorage(taskElement){
+let tasks = JSON.parse(localStorage.getItem("task"))||[]
+let taskText = taskElement.querySelector(".editable-input").value
+tasks = tasks.filter((t)=>t!==taskText)
+localStorage.setItem("tasks",JSON.stringify(tasks))
+
+}
+
+
+// load task from local storage
+function loadTaskFromLocalStorage() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => {
+    addTask(task, false); // calling the function addtask and add task to the UI
+  });
+}
+
+
 
 // eventlistner in submit button
 submitButton.addEventListener("click", () => {
   const text = taskAddition.value;
   console.log(text);
-  addTask(text);
+  addTask(text, true);
 });
 
 
+// eventlistner in submit button
+submitButton.addEventListener("click", () => {
+  const text = taskAddition.value;
+  console.log(text);
+  addTask(text, true);
+});
 
+
+//call the function localTaskFromLOcalStorage when page gets reload
+window.addEventListener("load", loadTaskFromLocalStorage);
